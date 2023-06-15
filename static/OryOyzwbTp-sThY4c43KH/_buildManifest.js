a02183f49439c07f1e37a5f631dfaa4a6eebecb4
syedzainulabdeen55 <syedzainulabdeen55@gmail.com>
2023-06-15 16:14:48 +0500

    Initial Release

---
+++ static/OryOyzwbTp-sThY4c43KH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/622-419744c53b0360a0.js","static/css/f464737620520f53.css","static/chunks/pages/index-779053968ccf09c9.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
